709be952f2730a1a5d28233c31b3b69fb472ffc3
Aman2104 <amanyara88@gmail.com>
2023-06-03 19:13:23 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import { BrowserRouter , Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import RegistrationForm from './components/Registration';
+import TransporterLandingPage from './components/TransporterLandingPage'
+import ManufacturerLandingPage from './components/ManufacturerLandingPage'
 function App() {
+  const [user, setUser] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  const storeTokenInCookie = (token) => {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 7);
+    document.cookie = `token=${token}; expires=${expirationDate.toUTCString()}; path=/; SameSite=Strict`;
+  };
+
+  const getTokenFromCookie = () => {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith('token=')) {
+        return cookie.substring('token='.length, cookie.length);
+      }
+    }
+    return null;
+  };
+
+  const token = getTokenFromCookie();
+
+  
+  const getUser = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/getuser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'auth-token': `${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setIsAuthenticated(true);
+        setUser(data);
+      } else {
+        throw new Error('Failed to get user');
+      }
+    } catch (error) {
+      console.error('Error getting user:', error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const token = getTokenFromCookie();
+    if (token) {
+      getUser();
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const verify=()=>{
+    setIsLoading(true);
+    const token = getTokenFromCookie();
+    if (token) {
+      getUser();
+    } else {
+      setIsLoading(false);
+    }
+  }
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? ( user.role==="Transporter"?
+                <TransporterLandingPage userType={user.role} token={token} />: <ManufacturerLandingPage userType={user.role} token={token}/>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route path="/login" element={<LoginPage storeTokenInCookie={storeTokenInCookie} verify={verify}/>} />
+          <Route path="/register" element={<RegistrationForm storeTokenInCookie={storeTokenInCookie} verify={verify}/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
